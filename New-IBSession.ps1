98f01ed4c63c012837bc7b89ca0297661ea38b4b
Daniel Ferreira <daniel@ferreira.fm>
2016-12-08 15:22:49 +0100

    Basic auth to IB web interface

--- New-IBSession.ps1
+++ New-IBSession.ps1
@@ -1,90 +1,60 @@
 Function New-IBSession {
     <#
     .SYNOPSIS
-        Generate Infoblox platform API connection information
+        Login to Infoblox platform by not using the API connection, but wrapper the http querys the site does to login the user when using a web browser. 
     
     .DESCRIPTION
-        Generate Infoblox platform API connection information
-    
-            Updates $IBConfig.IBSession - Infoblox Web Session
-            Updates $IBConfig.Uri       - Infoblox grid FQDN
-            Updates $IBConfig.IBVersion - Infoblox WAPI version 
-
-    .PARAMETER Credential
-        A valid PSCredential
-
-    .PARAMETER Passthru
-        If specified, we return the web session
-
-    .PARAMETER NoConfigChange
-        If specified, do not update the $IBConfig module variable or Infoblox.xml file
-
-    .PARAMETER Uri
-        The base Uri for the Infoblox.  Defaults to $IBConfig.Uri
-        Example: "https://grid.contoso.com"
-
-    .PARAMETER IBVersion
-        The WAPI version.  Defaults to $IBConfig.IBVersion
-        Example: "v1.6" or "v1.2"
+        This function was inspired in this one (API based): https://github.com/RamblingCookieMonster/Infoblox
+        Reason to not using the API it's because by the time of needing this project, API calls wasn't allowed. 
 
     .EXAMPLE
-        #Authenticate with the Infoblox. $IBConfig.IBSession and $IBConfig.Uri will now be populated
-        New-IBSession -Credential $Cred
+        New-IBSession -Uri https://grid.infobloxserver.com/ui -Credential (get-credential) -Passthru
 
-        #Get open session
-        Get-IBRange
+        Creates a Infoblox session with specific credential and passthru the credencial to the next pipeline cmdlet. 
 
-    .FUNCTIONALITY
-        Infoblox
+    .PARAMETER Uri
+        The base Uri for the Infoblox.
+
+    .PARAMETER Credential
+        A valid PSCredential.
+
+    .PARAMETER PassThru
+        If specified, returns the web session to the pipeline.
     #>   
-    [cmdletbinding()]
+    [CmdletBinding()]
+    [OutputType([psobject])]
     param(    
-        [string]$Uri = $Script:IBConfig.Uri,
-        [string]$IBVersion = $Script:IBConfig.IBVersion,
-        [System.Management.Automation.PSCredential]$Credential,
-        [switch]$Passthru,
-        [switch]$NoConfigChange
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [string] $Uri,
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [System.Management.Automation.PSCredential] $Credential,
+
+        [Parameter(Mandatory=$false)]
+        [switch] $PassThru
     )
 
-    #Build up URI
-    $BaseUri = Join-Parts $Uri "wapi/$IBVersion/grid"
-    
-    #Build up Invoke-RestMethod parameters for splatting
-        $IRMParams = @{
-            Uri = $BaseUri
-            Method = 'Get'
-            Credential = $Credential
-            SessionVariable = 'TempSession'
-            ErrorAction = 'Stop'
-        }
-   
-
-    Write-Verbose "Running $($MyInvocation.MyCommand).`nPSBoundParameters: $( $PSBoundParameters | Format-List | Out-String)`nInvoke-RestMethod parameters: $($IRMParams | Format-List | Out-String)"
-
-    Try
+    try
     {
-        #Run the command
-        $Grid = Invoke-RestMethod @IRMParams
-        $GridName = ( $Grid._ref -split ":" )[-1]
-        Write-Verbose "Connected to grid '$GridName'"
+        # Create the first GET request of login page
+        if (-not($Uri -match '/$')) { $Uri = $Uri+'/' }
+        $WebRequest = Invoke-WebRequest -Uri $Uri -Method Get -SessionVariable TempSession -ErrorAction Stop
+
+        # Filling the login form:
+        $WebRequest.Forms['loginForm'].fields['username'] = $credential.UserName
+        $WebRequest.Forms['loginForm'].fields['password'] = $credential.GetNetworkCredential().Password
+        $LoginToken = $WebRequest | Select -ExpandProperty InputFields | Where-Object { $_.outerHTML -match 'loginButton' } | Select onclick -Unique | ForEach-Object { [string](([regex]::Match($_.onclick,"Form\'\, '\.\/(?<form_id>[a-zA-Z0-9\-_]+)'\,")).groups["form_id"].value) }
+        Write-Verbose "Login token: $LoginToken"
+
+        Invoke-WebRequest -Uri ($Uri + $LoginToken) -Method Post -Body $WebRequest.Forms['loginForm'].Fields -WebSession $TempSession -ContentType application/x-www-form-urlencoded -ErrorAction Stop
     }
-    Catch
+    catch
     {
         Throw "Error retrieving session: $_"
     }
 
-    if($Passthru)
+    if($PassThru)
     {
         $TempSession
     }
-    
-    if(-not $NoConfigChange)
-    {
-        Set-IBConfig -Uri $Uri -IBVersion $IBVersion
-        
-        $Script:IBConfig.IBSession = $TempSession
-        $Script:IBConfig.Uri = $Uri
-        $Script:IBConfig.IBVersion = $IBVersion
-    }
-
 }
