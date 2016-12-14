0764793117e540853a45585019f85fa9dc11e4ee
Daniel Ferreira <daniel@ferreira.fm>
2016-12-14 18:35:24 +0100

    Adding first functionality of Get-IBDomainList function, and bugfixing

--- Get-IBDomainList.ps1
+++ Get-IBDomainList.ps1
@@ -1,4 +1,4 @@
-Function Get-IBDomain {
+Function Get-IBDomainList {
     <#
     .SYNOPSIS
         Get a list of all domains in DNS records of Infoblox.
@@ -15,16 +15,35 @@ Function Get-IBDomain {
     [CmdletBinding()]
     [OutputType([psobject])]
     param(    
-        
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [string] $Uri,
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [string] $BaseURL,
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [Microsoft.PowerShell.Commands.WebRequestSession] $WebSession
     )
 
 
     try
     {
+        $WebRequest = Invoke-WebRequest -Uri ($Uri + $BaseURL) -WebSession $WebSession -Method Get -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer)
+        $CallbackURL = [string](([regex]::Match($WebRequest.Content,"callbackUrl \: \'\.\.\/\.\.\/(?<callback_url>[a-zA-Z0-9\-_\/]+)\'\,")).groups["callback_url"].value)
+        if ($CallbackURL -eq $null) { throw }
+
+        $RandomNumber = Get-Random -Minimum 0.0 -Maximum 0.99; $RandomNumber = $RandomNumber -replace ',','.'
+        $DNSNetworkViewPostParameters = @{wicketAction='renderWicketComponent'; context='IBExt.context.DataManagement.DNS.Zones'; networkViewId='dns.network_view$0'; hierInfo='[{"objectId":"dns.network_view$0","objectType":"NetworkView","objectName":""},{"objectId":"dns.view$._default","objectType":"View","objectName":"default","scheduled":false,"isEditable":false}]'}
+        $WebRequest = Invoke-WebRequest -Uri ($Uri + $CallbackURL + "?random=$RandomNumber") -WebSession $WebSession -Method Post -Body $DNSNetworkViewPostParameters -Headers @{"Wicket-Ajax"="true"; "Wicket-Ajax-BaseURL"=$BaseURL; "Client-Date"="2016-12-14 15:55:38"; "Accept"="*/*"; "Accept-Encoding"="gzip, deflate, br"; "Origin"="https://144.199.214.139"; "Referer"="https://144.199.214.139/ui/JAJ_IQcrFuGI6y0cPUW-BA/JAJf2/IQc5a"} -ContentType application/x-www-form-urlencoded -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer)
+        $DNSNetworkViewURL = [string](([regex]::Match($WebRequest.Content,"\(\{ url\: \'\.\.\\/\.\.\\/(?<dns_networkview_url>[a-zA-Z0-9\-_\/\.\\]+PlD97)'\,")).groups["dns_networkview_url"].value) -Replace "\\",""
+        if ($DNSNetworkViewURL -eq $null) { throw }
+
         
+        $WebRequest = Invoke-RestMethod -Uri ($Uri + $DNSNetworkViewURL + "?_dc=1481726133272&page=first&sort=Ext2one&dir=DESC&jsonCommand=grid-data-load") -WebSession $WebSession -Method Get -Headers @{"Wicket-Ajax"="true"; "Wicket-Ajax-BaseURL"=$BaseURL} -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer) 
+        $WebRequest
     }
     catch
     {
-        throw "$_"
+        throw "Can't obtain data. $_"
     }
 }
--- New-IBSession.ps1
+++ New-IBSession.ps1
@@ -1,14 +1,14 @@
 Function New-IBSession {
     <#
     .SYNOPSIS
-        Login to Infoblox platform by not using the API connection, but wrapper the http querys the site does to login the user when using a web browser. 
+        Login to Infoblox platform by NOT using the API connection, but wrapper the http querys the site does to login the user when using a web browser. 
     
     .DESCRIPTION
         This function was inspired in this one (API based): https://github.com/RamblingCookieMonster/Infoblox
         Reason to not using the API it's because by the time of needing this project, API calls wasn't allowed. 
 
     .EXAMPLE
-        New-IBSession -Uri https://grid.infobloxserver.com/ui -Credential (get-credential) -Passthru
+        New-IBSession -Uri https://grid.infobloxserver.com/ui/ -Credential (get-credential)
 
         Creates a Infoblox session with specific credential and passthru the credencial to the next pipeline cmdlet. 
 
@@ -16,10 +16,7 @@ Function New-IBSession {
         The base Uri for the Infoblox.
 
     .PARAMETER Credential
-        A valid PSCredential.
-
-    .PARAMETER PassThru
-        If specified, returns the web session to the pipeline.
+        A valid PSCredential object obtained with Get-Credential.
     #>   
     [CmdletBinding()]
     [OutputType([psobject])]
@@ -33,19 +30,21 @@ Function New-IBSession {
 
     try
     {
-        # Create the first GET request of login page
+        # Create the first GET request of login page:
         if (-not($Uri -match '/$')) { $Uri = $Uri+'/' }
-        $WebRequest = Invoke-WebRequest -Uri $Uri -Method Get -SessionVariable LoginSession -ErrorAction Stop
+        $WebRequest = Invoke-WebRequest -Uri $Uri -Method Get -SessionVariable LoginSession -ErrorAction Stop -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer)
 
-        # Filling the login form:
+        # Filling the login form and submitting it:
+        $RandomNumber = Get-Random -Minimum 0.0 -Maximum 0.99; $RandomNumber = $RandomNumber -replace ',','.'
         $WebRequest.Forms['loginForm'].fields['username'] = $credential.UserName
         $WebRequest.Forms['loginForm'].fields['password'] = $credential.GetNetworkCredential().Password
         $LoginToken = $WebRequest | Select -ExpandProperty InputFields | Where-Object { $_.outerHTML -match 'loginButton' } | Select onclick -Unique | ForEach-Object { [string](([regex]::Match($_.onclick,"Form\'\, '\.\/(?<form_id>[a-zA-Z0-9\-_]+)'\,")).groups["form_id"].value) }
-        Write-Verbose "Login URL: $($Uri + $LoginToken)"
 
-        $WebRequest = Invoke-WebRequest -Uri ($Uri + $LoginToken) -Method Post -Body $WebRequest.Forms['loginForm'].Fields -WebSession $LoginSession -ErrorAction Stop -Headers @{"Wicket-Ajax"="true";"Wicket-Ajax-BaseURL"=".";"Wicket-FocusedElementId"="loginButton"}
+        $WebRequest = Invoke-WebRequest -Uri ($Uri + $LoginToken + "?random=$RandomNumber") -Method Post -Body $WebRequest.Forms['loginForm'].Fields -WebSession $LoginSession -ErrorAction Stop -Headers @{"Wicket-Ajax"="true";"Wicket-Ajax-BaseURL"="."} -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer)
         
-        if ($WebRequest.Headers['Ajax-Location'] -eq $null) { throw }
+        # Get Base URL id from Ajax-Location response header:
+        $BaseURL = [string](([regex]::Match($WebRequest.Headers['Ajax-Location'],"\.\/(?<base_url>[a-zA-Z0-9\-_\/]+)")).groups["base_url"].value)
+        if ($BaseURL -eq $null) { throw }
     }
     catch
     {
@@ -53,7 +52,8 @@ Function New-IBSession {
     }
 
     $ReturnObject = New-Object System.Object
-    $ReturnObject | Add-Member -Type NoteProperty -Name Uri -Value ($Uri + $WebRequest.Headers['Ajax-Location'])
+    $ReturnObject | Add-Member -Type NoteProperty -Name Uri -Value $Uri
+    $ReturnObject | Add-Member -Type NoteProperty -Name BaseURL -Value $BaseURL
     $ReturnObject | Add-Member -Type NoteProperty -Name Credential -Value $Credential
     $ReturnObject | Add-Member -Type NoteProperty -Name WebSession -Value $LoginSession
 
